a296cce092d47c39da71f3a5ae44e4d694231177
Robin <robin.skoogh@gmail.com>
2022-03-10 16:42:06 +0100

    filter search added to store page

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookStore.Controllers
@@ -16,9 +17,28 @@ namespace BookStore.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString, string minPrice, string maxPrice)
         {
-            return View(await _context.Books.ToListAsync());
+            var books = _context.Books.Select(b => b);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(b => b.Title.Contains(searchString) || b.Author.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(minPrice))
+            {
+                var min = int.Parse(minPrice);
+                books = books.Where(b => b.Price >= min);
+            }
+            
+            if (!string.IsNullOrEmpty(maxPrice))
+            {
+                var max = int.Parse(maxPrice);
+                books = books.Where(b => b.Price <= max);
+            }
+
+            return View(await books.ToListAsync());
         }
 
         public async Task<IActionResult> Details(int? id)
